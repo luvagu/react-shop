623bc1e5cda66da9a85be82bf2338bceca33137f
Luis Vallejo <luiavag@gmail.com>
2020-09-30 09:56:09 -0500

    created a the site header and links

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import Shop from './pages/shop/Shop';
 
 import './App.css';
 
+
 function App() {
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/shop' component={Shop} />
-        </Switch>
-      </Router>
-    </>
-    
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/shop' component={Shop} />
+      </Switch>
+    </Router>
   );
 }
 
