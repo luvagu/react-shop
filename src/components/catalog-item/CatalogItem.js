bd8d598a0e39023121a7fb1d69ee63576459db1b
Luis Vallejo <luiavag@gmail.com>
2020-09-30 06:34:55 -0500

    finished styling home catalog items

--- src/components/catalog-item/CatalogItem.js
+++ src/components/catalog-item/CatalogItem.js
@@ -3,7 +3,8 @@ import './CatalogItem.scss'
 
 function CategoryItem({ title, imageUrl, size }) {
     return (
-        <div className={`catalog-item ${size}`} style={{ backgroundImage: `url(${imageUrl})` }}>
+        <div className={`catalog-item ${size}`}>
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
