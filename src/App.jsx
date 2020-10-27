bbf89a6e1033a040f992b71aee774966258d86d6
Luis Vallejo <luiavag@gmail.com>
2020-10-27 11:45:06 -0500

    Logged in users are now redirected to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/user-actions'
@@ -50,15 +50,17 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/shop' component={Shop} />
-          <Route exact path='/signin' component={SigninRegister} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SigninRegister />) }  />
         </Switch>
       </>
     )
   }
 }
 
+const mapStateToProps = ({ user }) => ({ currentUser: user.currentUser })
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user-actions.js
+++ src/redux/user/user-actions.js
@@ -1,6 +1,6 @@
-import userReducer from "./user-reducer";
+import { userActionTypes } from "./user-types";
 
 export const setCurrentUser = (user) => ({
-    type: 'SET_CURRENT_USER',
+    type: userActionTypes.SET_CURRENT_USER,
     payload: user
 })
