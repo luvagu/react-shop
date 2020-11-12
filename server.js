287e7c104a47407aea9c8761e29c2dd22d6a118c
Luis Vallejo <luiavag@gmail.com>
2020-11-12 09:37:06 -0500

    separated the app into client and server to be able to process the stripe backend payment charge

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+// @TODO - remove
+// require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    })
+
+    // app.get('/', (req, res) => {
+    //     res.send('<h1>Helloooo 🚀</h1>')
+    // })
+}
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    }
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (err) {
+            res.status(500).send({ error: stripeErr })
+        } else {
+            res.status(200).send({ success: stripeRes })
+        }
+    })
+})
+
+// @TODO - remove
+// app.get('/payment', (req, res) => {
+//     res.status(200).json({ success: 'hi' })
+// })
+
+app.listen(port, (err) => {
+    if (err) throw err
+    console.log('Server running on port ' + port)
+})
--- src/redux/catalog/catalog-selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect'
-
-const selectCatalog = (state) => state.catalog
-
-export const selectCatalogSection = createSelector(
-    [selectCatalog],
-    (catalog) => catalog.categories
-)
