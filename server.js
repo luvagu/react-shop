28e4a6aeacf1c7e2a54f4c7fce1468b840ae9c9c
Luis Vallejo <luiavag@gmail.com>
2020-11-12 09:38:37 -0500

    separated the app into client and server to be able to process the stripe backend payment charge

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 
     // app.get('/', (req, res) => {
