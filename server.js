97554b0bc7a5b0c1d68c2219197a639bad02bd46
Luis Vallejo <luiavag@gmail.com>
2020-11-12 10:59:50 -0500

    separated the app into client and server to be able to process the stripe backend payment charge

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded())
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
